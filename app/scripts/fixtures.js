fbdfe06036bd0d84eb9b103faaa6997f2d8ecbc9
Tom Opdebeeck <tom230691@gmail.com>
2017-08-02 15:20:44 +0200

    update role permissions + cleanup fixtures script
    * reuse runqueue
    * find all admin roles and add/update contenttypes

--- app/scripts/fixtures.js
+++ app/scripts/fixtures.js
@@ -42,38 +42,75 @@ function connect() {
 	}
 }
 
+function runQueue(queue) {
+	var result = Q.resolve();
+
+	queue.forEach(function(update, i) {
+		result = result.then(function() {
+			return update(i);
+		});
+	});
+
+	return result;
+}
+
+function updateContentItem(type, model) {
+	return model.findOneAndUpdate({
+		uuid: type.uuid,
+	}, type, {
+		upsert: true,
+		new: true,
+	})
+	.exec();
+}
+
+function updatePermissions(type, model) {
+	return model
+		.find({ isAdmin: true })
+		.exec()
+		.then(function(items) {
+			var typeId = type._id.toString();
+
+			return runQueue(items.map(function(item) {
+				return function() {
+					var typeIndex = _.findIndex(item.contentTypes, { type: typeId });
+
+					typeIndex = typeIndex > 0 ? typeIndex : item.contentTypes.length - 1;
+
+					item.contentTypes.splice(typeIndex, 1, {
+						type: typeId,
+						permissions: {
+							delete: true,
+							update: true,
+							read: true,
+							create: false,
+						},
+					});
+
+					return model.update({ _id: item._id }, item);
+				};
+			}));
+		}, function(err) {
+			throw err;
+		});
+}
+
 function updateForms() {
 	connect();
 
 	var ContentTypeModel = require("app/models/contentType");
+	var RoleModel = require("app/models/role");
 	var forms = require("../fixtures/forms");
 	var formTypes = Object.keys(forms);
 
-	var updates = formTypes.map(function() {
-		return function(type) {
-			return ContentTypeModel.findOneAndUpdate({
-				uuid: type.uuid,
-			}, type, {
-				upsert: true,
-				new: true,
-			})
-			.exec();
+	runQueue(formTypes.map(function() {
+		return function(i) {
+			var type = forms[formTypes[i]];
+
+			return updateContentItem(type, ContentTypeModel).then(updatePermissions.bind(null, type, RoleModel));
 		};
-	});
-
-	function runQueue(queue) {
-		var result = Q.resolve();
-
-		queue.forEach(function(update, i) {
-			result = result.then(function() {
-				return update(forms[formTypes[i]]);
-			});
-		});
-
-		return result;
-	}
-
-	runQueue(updates).then(function() {
+	}))
+	.then(function() {
 		console.log("ALL TYPES UPDATED");
 		process.exit(0);
 	}, function(err) {

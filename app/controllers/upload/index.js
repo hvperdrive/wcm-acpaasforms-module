67bce12eda7787aca03b07dc1ad4193e667102b9
Tom Opdebeeck <tom230691@gmail.com>
2017-08-08 13:38:03 +0200

    add multer diskstorage config

--- app/controllers/upload/index.js
+++ app/controllers/upload/index.js
@@ -1,8 +1,10 @@
+var multer = require("./multer");
 var get = require("./get");
 var remove = require("./remove");
 var upload = require("./upload");
 
 module.exports = {
+	multer: multer,
 	get: get,
 	remove: remove,
 	upload: upload,
---
+++ app/controllers/upload/multer.js
@@ -0,0 +1,12 @@
+var multer = require("multer");
+
+var storage = multer.diskStorage({
+	destination: "/tmp",
+	filename: function(req, file, cb) {
+		cb(null, file.fieldname + "-" + Date.now());
+	}
+});
+
+module.exports = multer({
+	storage: storage,
+});

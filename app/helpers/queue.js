4a8495f1d413a4a796bee064def99375ad87b309
Tom Opdebeeck <tom230691@gmail.com>
2017-08-08 16:57:19 +0200

    add queue helper

---
+++ app/helpers/queue.js
@@ -0,0 +1,24 @@
+var Q = require("q");
+
+module.exports = function runQueue(queue) {
+	var result = Q.resolve();
+	var results = [];
+
+	queue.forEach(function(update, i) {
+		result = result.then(function(response) {
+			if (i > 0) {
+				results.push(response);
+			}
+
+			return update(i);
+		});
+	});
+
+	return result.then(function(response) {
+		results.push(response);
+
+		return results;
+	}, function(err) {
+		throw err;
+	});
+};

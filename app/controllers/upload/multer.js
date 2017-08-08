19ed976a186acd16f12a559226e7791f39704143
Tom Opdebeeck <tom230691@gmail.com>
2017-08-08 15:05:29 +0200

    proper filenames

--- app/controllers/upload/multer.js
+++ app/controllers/upload/multer.js
@@ -1,9 +1,10 @@
 var multer = require("multer");
+var fileHelpers = require("./helpers/file");
 
 var storage = multer.diskStorage({
 	destination: "/tmp",
 	filename: function(req, file, cb) {
-		cb(null, file.fieldname + "-" + Date.now());
+		cb(null, fileHelpers.filename(file.originalname) + "-" + Date.now() + fileHelpers.extension(file.originalname));
 	},
 });
 

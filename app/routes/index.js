a414e468192e4f0aa017f1df42d9581876d6028b
Tom Opdebeeck <tom230691@gmail.com>
2017-08-22 13:54:32 +0200

    cleanup

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,4 @@
-var listeners = require("../controllers/listeners");
-
 module.exports = function(app, hooks) {
-	// Setup listeners
-	listeners.start();
-
 	// Setup hooks
 	require("../controllers/hooks")(hooks);
 };

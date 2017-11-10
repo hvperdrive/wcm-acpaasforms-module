456d01a570e462205da9465b3d8f9de32ad4092a
jeroenvalcke <valcke_jeroen@hotmail.com>
2017-11-10 15:11:55 +0100

    Call next on verifyLogin error

--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -5,12 +5,14 @@ var _ = require("lodash");
 var getModule = require("@wcm/module-helper").getModule;
 
 module.exports.verifyLogin = function verifyLogin(req, res, next) {
-	return getModule("@wcm/member")
+	return getModule("@wcm/members")
 		.then(function(mod) {
 			if (typeof _.get(mod, "memberAccessMiddleware.hard") !== "function") {
 				return next();
 			}
 
 			return mod.memberAccessMiddleware.hard(req, res, next);
+		}, function() {
+			return next();
 		});
 };
